d177a5c38ed229096f2e56502d55238d840dba24
Nikolas Silva <nikolas.rsilva@gmail.com>
2022-03-29 20:37:05 -0300

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,1 @@
+<h1>sketches! sketches! sketches!</h1>
---
+++ svelte.config.js
@@ -0,0 +1,18 @@
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.env.NODE_ENV === 'development';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter(),
+		prerender: {
+			default: true,
+		},
+		paths: {
+			base: dev ? '' : '/sketches',
+		},
+	},
+};
+
+export default config;
