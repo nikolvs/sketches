9c19fe3f407ea5d3f08e6d38e67f741395ae902e
Nikolas Silva <nikolas.rsilva@gmail.com>
2022-06-02 02:25:08 -0300

    Add tailwindcss

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
 
 const dev = process.env.NODE_ENV === 'development';
 
@@ -13,6 +14,11 @@ const config = {
       base: dev ? '' : '/sketches',
     },
   },
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
 };
 
 export default config;
